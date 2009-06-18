d542a5db13eb87bd668ad15fed7d1e43380f3d8d
James Coglan <jcoglan@googlemail.com>
2009-06-18 11:27:42 +0100

    Initial basic functionality for named events.

--- lib/eventful.rb
+++ lib/eventful.rb
@@ -1,3 +1,44 @@
-class Eventful
+require 'observer'
+
+module Eventful
   VERSION = '0.9.0'
+  
+  class Observer
+    def initialize(&block)
+      @block = block
+    end
+    
+    def update(*args)
+      @block.call(*args)
+    end
+  end
+  
+  module ObservableWithBlocks
+    include Observable
+    
+    def add_observer(*args, &block)
+      return super unless block_given?
+      observer = Observer.new(&block)
+      add_observer(observer)
+      observer
+    end
+  end
+  
+  include ObservableWithBlocks
+  
+  def on(event, &block)
+    add_observer do |*args|
+      type, data = args.first, [self] + args[1..-1]
+      block.call(*data) if type == event
+    end
+  end
+  
+  def fire(*args)
+    return if defined?(@observer_state) and not @observer_state
+    changed(true)
+    notify_observers(*args)
+    changed(true)
+  end
+  
 end
+
--- test/test_eventful.rb
+++ test/test_eventful.rb
@@ -1,8 +1,27 @@
 require "test/unit"
 require "eventful"
 
+class Foo
+  include Eventful
+end
+
 class TestEventful < Test::Unit::TestCase
   def test_sanity
-    flunk "write tests or I will kneecap you"
+    ayes, noes = 0, 0
+    f = Foo.new
+    f.on(:aye) { |foo, x| ayes += x }
+    obs = f.on(:noe) { |foo, x| noes += x }
+    
+    f.fire(:aye, 1)
+    assert_equal 1, ayes
+    assert_equal 0, noes
+    f.fire(:noe, 3)
+    assert_equal 1, ayes
+    assert_equal 3, noes
+    
+    f.delete_observer(obs)
+    f.fire(:noe, 3)
+    assert_equal 1, ayes
+    assert_equal 3, noes
   end
 end
